ca9a575d06de486ebe5bbf91c047732f84b48c30
Wouter Vellekoop <woutervellekoop@Wouters-MacBook-Pro-2.local>
2025-07-03 15:47:50 +0200

    Initial commit

---
+++ wp-content/themes/bridge/css/style_dynamic_callback.php
@@ -0,0 +1,752 @@
+
+
+
+
+	body{
+		 color: #0a0a0a; 			}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+						.content{
+			margin-top: -100px;
+			}
+		
+	
+
+
+
+
+		header.fixed_hiding .q_logo a,
+		header.fixed_hiding .q_logo{
+		max-height: 100px;
+		}
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	p,
+    body.qode-overridden-elementors-fonts .elementor-widget-heading p.elementor-heading-title {
+	color: #0a0a0a;								}
+	.filter_holder ul li span,
+	blockquote h5,
+	.q_social_icon_holder .simple_social,
+	.header-widget.widget_nav_menu ul.menu li a,
+	.side_menu a,
+	.side_menu li,
+	.side_menu span,
+	.side_menu p,
+	.side_menu .widget.widget_rss li a.rsswidget,
+	.side_menu #wp-calendar caption,
+	.side_menu #wp-calendar th,
+	.side_menu #wp-calendar td,
+	aside .widget #lang_sel_list li a,
+	aside .widget #lang_sel li a,
+	aside .widget #lang_sel_click li a,
+	.wpb_widgetised_column .widget #lang_sel_list li a,
+	.wpb_widgetised_column .widget #lang_sel li a,
+	.wpb_widgetised_column .widget #lang_sel_click li a,
+    .elementor-widget.elementor-widget-sidebar .widget #lang_sel_list li a,
+	.elementor-widget.elementor-widget-sidebar .widget #lang_sel li a,
+	.elementor-widget.elementor-widget-sidebar .widget #lang_sel_click li a,
+	section.side_menu #lang_sel_list li a,
+	section.side_menu #lang_sel li a,
+	section.side_menu #lang_sel_click li a,
+	footer #lang_sel_list li a,
+	footer #lang_sel li a,
+	footer #lang_sel_click li a,
+	footer #lang_sel_list.lang_sel_list_horizontal a,
+	footer #lang_sel_list.lang_sel_list_vertical a,
+	.side_menu #lang_sel_list.lang_sel_list_horizontal a,
+	.side_menu #lang_sel_list.lang_sel_list_vertical a,
+	#lang_sel_footer a{
+	color: #0a0a0a;	}
+	.header_top #lang_sel > ul > li > a,
+	.header_top #lang_sel_click > ul > li> a,
+	footer #lang_sel ul li a,
+	footer #lang_sel ul ul a,
+	footer #lang_sel_click ul li a,
+	footer #lang_sel_click ul ul a,
+	footer #lang_sel_click ul ul a span,
+	section.side_menu #lang_sel ul li a,
+	section.side_menu #lang_sel ul ul a,
+	section.side_menu #lang_sel ul ul a:visited,
+	section.side_menu #lang_sel_click > ul > li > a,
+	section.side_menu #lang_sel_click ul ul a,
+	section.side_menu #lang_sel_click ul ul a:visited{
+	color: #0a0a0a !important;	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		.google_map{
+		height: 750px;
+		}
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+	.footer_top,
+	.footer_top .footer_col1,
+	.footer_top .container_inner > .widget,
+	.footer_top.footer_top_full > .widget{
+	text-align: center;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*Blog Masonry Gallery - end */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	.vertical_menu_float .menu-item .second{
+	left: calc(100% + 30px); /*because of the padding*/
+	}
+
+	.vertical_menu_hidden aside.vertical_menu_area .vertical_menu_float .menu-item .second {
+	left: calc(100% + 40px);
+	}
+
+
+
+
+
+
+
+
+	.vertical_menu_area{
+	text-align:left;
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	.portfolio_main_holder .item_holder.thin_plus_only .thin_plus_only_icon {
+	font-family: 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	.masonry_gallery_item.square_big .masonry_gallery_item_inner .masonry_gallery_item_content{
+	text-align: center;	}
+
+
+	.masonry_gallery_item.square_small .masonry_gallery_item_inner .masonry_gallery_item_content{
+	text-align: center;	}
+
+
+	.masonry_gallery_item.rectangle_portrait .masonry_gallery_item_inner .masonry_gallery_item_content{
+	text-align: center;	}
+
+
+	.masonry_gallery_item.rectangle_landscape .masonry_gallery_item_inner .masonry_gallery_item_content{
+	text-align: center;	}
+
+
+
+
+
+
+
+
+.qode-type1-gradient-left-to-right { background: linear-gradient(to right,#5a912d, #5a912d);}
+
+.qode-type1-gradient-bottom-to-top, .qode-type1-gradient-bottom-to-top-after:after { background: linear-gradient(to top,#5a912d, #5a912d);}
+
+.qode-type1-gradient-left-bottom-to-right-top { background: linear-gradient(to right top,#5a912d, #5a912d);}
+
+.qode-type1-gradient-left-to-right-2x { background: linear-gradient(to right,#5a912d 0%, #5a912d 50%,#5a912d 100%);background-size: 200% 200%;}
+
+.qode-type1-gradient-left-to-right-text i, .qode-type1-gradient-left-to-right-text i:before, .qode-type1-gradient-left-to-right-text span { background: -webkit-linear-gradient(right top,#5a912d, #5a912d);color: #5a912d;-webkit-background-clip: text;-webkit-text-fill-color: transparent;}
+
+.qode-type1-gradient-bottom-to-top-text i, .qode-type1-gradient-bottom-to-top-text i:before, .qode-type1-gradient-bottom-to-top-text span, .qode-type1-gradient-bottom-to-top-text span span { background: -webkit-linear-gradient(bottom,#5a912d, #5a912d);color: #5a912d;-webkit-background-clip: text;-webkit-text-fill-color: transparent;}
+
+.qode-type1-gradient-bottom-to-top-text-hover:hover i, .qode-type1-gradient-bottom-to-top-text-hover:hover i:before, .qode-type1-gradient-bottom-to-top-text-hover:hover span, .qode-type1-gradient-bottom-to-top-text-hover:hover span span { background: -webkit-linear-gradient(bottom,#5a912d, #5a912d);color: #5a912d;-webkit-background-clip: text;-webkit-text-fill-color: transparent;}
+
+
+				
+		
+		
+								
+					body{
+						 color: #0a0a0a; 									}
+											
+		
+		
+		
+																
+		
+
+
+		
+						
+		
+		
+		
+
+														.content{
+					margin-top: -100px;
+					}
+				
+					
+								
+		
+		
+		
+						header.fixed_hiding .q_logo a,
+				header.fixed_hiding .q_logo{
+				max-height: 100px;
+				}
+				
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+
+		
+
+																		
+
+						
+		
+		
+		
+		
+		
+				
+						
+		
+		
+		
+				
+		
+																
+
+					p,
+            body.qode-overridden-elementors-fonts .elementor-widget-heading p.elementor-heading-title {
+			color: #0a0a0a;																								}
+			.filter_holder ul li span,
+			blockquote h5,
+			.q_social_icon_holder .simple_social,
+			.header-widget.widget_nav_menu ul.menu li a,
+			.side_menu a,
+			.side_menu li,
+			.side_menu span,
+			.side_menu p,
+			.side_menu .widget.widget_rss li a.rsswidget,
+			.side_menu #wp-calendar caption,
+			.side_menu #wp-calendar th,
+			.side_menu #wp-calendar td,
+			aside .widget #lang_sel_list li a,
+			aside .widget #lang_sel li a,
+			aside .widget #lang_sel_click li a,
+			.wpb_widgetised_column .widget #lang_sel_list li a,
+			.wpb_widgetised_column .widget #lang_sel li a,
+			.wpb_widgetised_column .widget #lang_sel_click li a,
+            .elementor-widget.elementor-widget-sidebar .widget #lang_sel_list li a,
+            .elementor-widget.elementor-widget-sidebar .widget #lang_sel li a,
+            .elementor-widget.elementor-widget-sidebar .widget #lang_sel_click li a,
+			section.side_menu #lang_sel_list li a,
+			section.side_menu #lang_sel li a,
+			section.side_menu #lang_sel_click li a,
+			footer #lang_sel_list li a,
+			footer #lang_sel li a,
+			footer #lang_sel_click li a,
+			footer #lang_sel_list.lang_sel_list_horizontal a,
+			footer #lang_sel_list.lang_sel_list_vertical a,
+			.side_menu #lang_sel_list.lang_sel_list_horizontal a,
+			.side_menu #lang_sel_list.lang_sel_list_vertical a,
+			#lang_sel_footer a{
+			color: #0a0a0a;			}
+			.header_top #lang_sel > ul > li > a,
+			.header_top #lang_sel_click > ul > li> a,
+			footer #lang_sel ul li a,
+			footer #lang_sel ul ul a,
+			footer #lang_sel_click ul li a,
+			footer #lang_sel_click ul ul a,
+			footer #lang_sel_click ul ul a span,
+			section.side_menu #lang_sel ul li a,
+			section.side_menu #lang_sel ul ul a,
+			section.side_menu #lang_sel ul ul a:visited,
+			section.side_menu #lang_sel_click > ul > li > a,
+			section.side_menu #lang_sel_click ul ul a,
+			section.side_menu #lang_sel_click ul ul a:visited{
+			color: #0a0a0a !important;			}
+									
+
+		
+				
+				
+		
+		
+
+		
+		
+		
+										
+		
+		
+		
+						.google_map{
+				height: 750px;
+				}
+				
+		
+		
+		
+								
+		
+		
+		
+		
+
+		
+		
+		
+		
+					.footer_top,
+			.footer_top .footer_col1,
+			.footer_top .container_inner > .widget,
+			.footer_top.footer_top_full > .widget{
+			text-align: center;
+			}
+		
+		
+		
+		
+		
+		
+		
+
+		
+
+		
+		
+
+												
+										
+								
+								
+										
+										
+								
+								
+		
+														
+								
+								
+								
+				
+						
+						
+		/*Blog Masonry Gallery - end */
+
+								
+						
+						
+								
+												
+								
+
+		
+						
+				
+		
+
+		
+				
+
+										
+		
+
+												
+				
+				
+		
+								
+								
+								
+		
+																																																				
+				
+		
+		.vertical_menu_float .menu-item .second{
+		left: calc(100% + 30px); /*because of the padding*/
+		}
+
+		.vertical_menu_hidden aside.vertical_menu_area .vertical_menu_float .menu-item .second {
+		left: calc(100% + 40px);
+		}
+
+
+		
+		
+		
+		
+
+
+							.vertical_menu_area{
+			text-align:left;
+			}
+			
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+																													.portfolio_main_holder .item_holder.thin_plus_only .thin_plus_only_icon {
+			font-family: 			}
+								
+		
+		
+				
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+
+		
+		
+
+		
+		
+		
+								
+		
+		
+				
+				
+				
+				
+										
+
+		
+		
+																
+
+						
+		
+
+		
+				    .masonry_gallery_item.square_big .masonry_gallery_item_inner .masonry_gallery_item_content{
+			text-align: center;		    }
+		
+		
+				    .masonry_gallery_item.square_small .masonry_gallery_item_inner .masonry_gallery_item_content{
+			text-align: center;		    }
+		
+		
+				    .masonry_gallery_item.rectangle_portrait .masonry_gallery_item_inner .masonry_gallery_item_content{
+			text-align: center;		    }
+		
+		
+				    .masonry_gallery_item.rectangle_landscape .masonry_gallery_item_inner .masonry_gallery_item_content{
+			text-align: center;		    }
+		
+
+	
+						
+		
+		
+
+				
